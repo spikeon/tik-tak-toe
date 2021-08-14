d8af20bb339dd30b424c8f21cfcdd0e1f16cbb18
Mike Flynn <mflynn@quintron.com>
2021-08-14 16:47:08 -0400

    TTT-T-1:
    - Booting up a fresh React App using Yarn and then removing the default react stuff.
    - Adding styled-components library
    - Adding a small example component that exercises all of the functionality of react that the final app will need to use; Page, Component, Parameters both method and value, state, and styled.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GamePage from "./pages/game/game.component";
+
+function App() {
+  return (
+    <div className="App">
+      <GamePage />
+    </div>
+  );
+}
+
+export default App;
